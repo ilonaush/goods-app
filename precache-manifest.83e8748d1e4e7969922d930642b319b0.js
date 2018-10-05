6d82b98d6299b54c425ed0ab399201211940dccf
ilonaush <ushakova.ilona@gmail.com>
2018-10-05 12:11:37 +0300

    Updates

---
+++ precache-manifest.83e8748d1e4e7969922d930642b319b0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "a736de5b8fc8da9f1d17fed049449a32",
+    "url": "/goods-app/static/media/background.a736de5b.jpg"
+  },
+  {
+    "revision": "9807fbc8105b49275a91",
+    "url": "/goods-app/static/js/runtime~main.9807fbc8.js"
+  },
+  {
+    "revision": "7a1b0c4b2ca1e75d5743",
+    "url": "/goods-app/static/js/main.7a1b0c4b.chunk.js"
+  },
+  {
+    "revision": "73bf638a76be2771df04",
+    "url": "/goods-app/static/js/1.73bf638a.chunk.js"
+  },
+  {
+    "revision": "7a1b0c4b2ca1e75d5743",
+    "url": "/goods-app/static/css/main.cccf9c29.chunk.css"
+  },
+  {
+    "revision": "21dbe0fac122db7d0eac431af9effbf2",
+    "url": "/goods-app/index.html"
+  }
+];
